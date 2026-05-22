ae1bdd018145eec3b34f9f320618d71b8d8f54d1
Jacob Tan <6391234+jacobtyq@users.noreply.github.com>
2026-05-22 17:59:41 +0800

    feat: skip tests with @visual in CI

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('home – desktop light', async ({ page }) => {
+test('home – desktop light @visual', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveScreenshot('home-desktop-light.png', { fullPage: true });
 });
 
-test('home – mobile light', async ({ page }) => {
+test('home – mobile light @visual', async ({ page }) => {
   await page.setViewportSize({ width: 375, height: 812 });
   await page.goto('/');
   await expect(page).toHaveScreenshot('home-mobile-light.png', { fullPage: true });
