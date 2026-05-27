7dd24ac98e954619d7e056eb071692abd2c09f66
Jacob Tan <6391234+jacobtyq@users.noreply.github.com>
2026-05-27 15:56:12 +0800

    feat: update blurb, add sidenav

--- src/content.config.ts
+++ src/content.config.ts
@@ -22,11 +22,9 @@ const blurb = defineCollection({
   schema: ({ image }) =>
     z.object({
       title: z.string(),
-      description: z.string().optional(),
       // Transform string to Date object
       pubDate: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
-      heroImage: z.optional(image()),
     }),  
 })
 
